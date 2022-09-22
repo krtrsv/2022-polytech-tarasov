4e987d2281d7bd763f71fcf630dfb244ca2043ce
Kirill Tarasov <tarasov.ks@edu.spbstu.ru>
2022-09-22 17:31:40 +0300

    feat(calc): yet another useless commit

---
+++ 03_cpp/calculator/flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        calculator = (with pkgs; stdenv.mkDerivation {
+          pname = "calculator";
+          version = "1.2.0";
+          src = ./.;
+          nativeBuildInputs = [
+            cmake
+            clang
+          ];
+          buildPhase = ''
+            make -j $NIX_BUILD_CORES
+          '';
+          installPhase = ''
+            mkdir -p $out/bin/
+            mv calculator $out/bin/
+          '';
+        });
+      in rec {
+        defaultApp = flake-utils.lib.mkApp {
+          drv = defaultPackage;
+        };
+        defaultPackage = calculator;
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            calculator
+          ];
+        };
+      }
+    );
+}
