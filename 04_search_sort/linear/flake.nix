1757ae54d5593c2f1fad813930c0cf10c4fc9b6b
Kirill Tarasov <tarasov.ks@edu.spbstu.ru>
2022-09-29 16:25:45 +0300

    feat(linear): flake

---
+++ 04_search_sort/linear/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShell = pkgs.mkShell rec {
+        name = "linear-search";
+        packages = with pkgs; [
+          cmake
+          clang
+        ];
+      };
+    }
+  );
+}
