5917d980e3beb73edc169f0b6b7137d1419ce378
Kirill Tarasov <tarasov.ks@edu.spbstu.ru>
2022-09-29 17:25:30 +0300

    feat(array-insertion): add `search_position`

--- 04_search_sort/array_insertion/array-insertion.cpp
+++ 04_search_sort/array_insertion/array-insertion.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 
 int *insert(int arr[], int n, int x, int p);
+int search_position(int arr[], int n, int x);
 
 int main() {
   // NOTE: aN - array
@@ -14,6 +15,13 @@ int main() {
   for (auto i = 0; i < 11; ++i)
     assert(r0[i] == expected_r0[i]);
 
+  // `search_position` example
+  int a1[4] = {1, 3, 5, 6};
+  assert(search_position(a1, 4, 5) == 2);
+
+  int a2[4] = {1, 3, 5, 6};
+  assert(search_position(a2, 4, 7) == 4);
+
   return 0;
 }
 
@@ -23,4 +31,12 @@ int *insert(int arr[], int n, int x, int p) {
 
   arr[p] = x;
   return arr;
+}
+
+int search_position(int arr[], int n, int x) {
+  for (auto i = 0; i < n - 1; ++i) {
+    if (arr[i] >= x)
+      return i;
+  }
+  return n;
 }
