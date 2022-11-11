bafd7bc23ca5be5a96c32efa65a0be7deaca4532
Kirill Tarasov <tarasov.ks@edu.spbstu.ru>
2022-11-11 10:10:15 +0300

    feat: update 08

--- 08_oop/intro/String.cpp
+++ 08_oop/intro/String.cpp
@@ -1,25 +1,19 @@
-#include <iostream>
-#include <cstring>
 #include "String.h"
+#include <cstring>
+#include <iostream>
 
 String::String(const char *s) {
-  data = (char*)malloc(strlen(s));
+  length = strlen(s);
+  data = (char *)malloc(length);
   strcpy(data, s);
 }
 
-unsigned int String::get_length() {
-  if (!data)
-    return 0;
-  const char *end = data;
-  for (; *end != '\0'; ++end)
-    ;
-  return end - data;
-}
+unsigned int String::get_length() { return length; }
 
 const char *String::c_str() { return data; }
 
 String String::operator+(String rv) {
-  auto dest = (char*)malloc(this->get_length());
+  auto dest = (char *)malloc(length);
   strcpy(dest, data);
   strcpy(dest + rv.get_length(), rv.c_str());
   return String(dest);
--- 08_oop/intro/main.cpp
+++ 08_oop/intro/main.cpp
@@ -1,7 +1,7 @@
 #include "String.h"
 #include <iostream>
 
-int main () {
+int main() {
   String foo = "help";
   String bar = foo + " pls";
   std::cout << bar << '\n' << bar.get_length();
