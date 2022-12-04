ccdb81b86942ff2a8bb8e9b14bebf9da25ee4064
Kirill Tarasov <tarasov.ks@edu.spbstu.ru>
2022-12-05 00:58:59 +0300

    fixt(task-09): i have autism

--- 09_oop/json/json.hpp
+++ 09_oop/json/json.hpp
@@ -2,24 +2,28 @@
 #include <string>
 #include <unordered_map>
 
-struct BasicToken {
+struct IJsonToken {
   std::string value;
 };
 
-struct StringToken : public BasicToken {
-  StringToken(std::string s) { value = '"' + s + '"'; }
+struct JsonString : public IJsonToken {
+  JsonString(std::string s) { value = '"' + s + '"'; }
 };
-struct NumToken : BasicToken {
-  template <typename T> NumToken(T i) { value = std::to_string(i); }
+
+struct JsonNumber : IJsonToken {
+  template <typename T> JsonNumber(T i) { value = std::to_string(i); }
 };
-struct BoolToken : BasicToken {
-  BoolToken(bool b) { value = b ? "true" : "false"; }
+
+struct JsonBool : IJsonToken {
+  JsonBool(bool b) { value = b ? "true" : "false"; }
 };
-struct ArrayToken : BasicToken {
-  ArrayToken(std::initializer_list<BasicToken> v) {
+
+struct JsonArray : IJsonToken {
+  JsonArray(std::initializer_list<IJsonToken> v) {
     value = "[";
     for (std::size_t i = 1; auto s : v) {
       value.append(s.value);
+      // i hate it
       if (i != v.size()) {
         value.append(",");
         i++;
@@ -30,9 +34,13 @@ struct ArrayToken : BasicToken {
 };
 
 struct Json {
-  std::unordered_map<std::string, BasicToken> tokens;
+  std::unordered_map<std::string, IJsonToken> tokens = {};
 
-  Json(std::unordered_map<std::string, BasicToken> j) { tokens = j; }
+  Json(std::initializer_list<std::pair<std::string, IJsonToken>> il) {
+    for (auto it : il)
+      tokens.insert_or_assign(it.first, it.second);
+  }
+
   std::string serialize() {
     std::string out = "{";
     for (std::size_t i = 1; const auto &elem : tokens) {
--- 09_oop/json/test_json.cpp
+++ 09_oop/json/test_json.cpp
@@ -1,41 +1,28 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "json.hpp"
+
 #include <doctest.h>
-#include <iostream>
-#include <unordered_map>
 
-TEST_CASE("Generic tests") {
-  CHECK_EQ(Json{std::unordered_map<std::string, BasicToken>{}}.serialize(),
-           "{}");
 
-  CHECK_EQ(
-      Json{std::unordered_map<std::string, BasicToken>{
-               {"int_token", NumToken(9090)},
-               {"str_token", StringToken("hi")},
-               {"bool_token", BoolToken(true)},
-               {"array_token", ArrayToken({NumToken(1), StringToken("ok")})}}}
-          .serialize(),
-      "{\"bool_token\":true,\"str_token\":\"hi\",\"array_token\":[1,\"ok\"],"
-      "\"int_token\":9090}");
+TEST_CASE("All JSON types can be serialized") {
+  CHECK_EQ(Json{}.serialize(), "{}");
 
-  CHECK_EQ(Json{std::unordered_map<std::string, BasicToken>{
-                    {"str_token", StringToken("val")}}}
-               .serialize(),
+  CHECK_EQ(Json{{"str_token", JsonString("val")}}.serialize(),
            "{\"str_token\":\"val\"}");
 
-  CHECK_EQ(Json{std::unordered_map<std::string, BasicToken>{
-                    {"int_token", NumToken(999999)}}}
-               .serialize(),
+  CHECK_EQ(Json{{"int_token", JsonNumber(999999)}}.serialize(),
            "{\"int_token\":999999}");
 
-  CHECK_EQ(Json{std::unordered_map<std::string, BasicToken>{
-                    {"bool_token", BoolToken(true)}}}
-               .serialize(),
+  CHECK_EQ(Json{{"bool_token", JsonBool(true)}}.serialize(),
            "{\"bool_token\":true}");
 
-  CHECK_EQ(
-      Json{std::unordered_map<std::string, BasicToken>{
-               {"array_token", ArrayToken({NumToken(1), StringToken("ok")})}}}
-          .serialize(),
-      "{\"array_token\":[1,\"ok\"]}");
+  CHECK_EQ(Json{{"array_token", JsonArray({JsonNumber(1), JsonString("ok")})}}
+               .serialize(),
+           "{\"array_token\":[1,\"ok\"]}");
+}
+
+TEST_CASE("Duplicates update values") {
+  CHECK_EQ(Json{{"a", JsonNumber(999999)}, {"a", JsonNumber(0)}}.serialize(),
+           "{\"a\":0}");
 }
